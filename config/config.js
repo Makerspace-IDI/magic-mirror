4a0e965ec4011b7bc937cebd1137fc722a0152f7
Håvard Anda Estensen <haavard.ae@gmail.com>
2016-11-09 15:05:04 +0100

    Remove all but clock

--- config/config.js
+++ config/config.js
@@ -18,53 +18,7 @@ var config = {
 		{
 			module: 'clock',
 			position: 'top_left'
-		},
-		{
-			module: 'calendar',
-			header: 'US Holidays',
-			position: 'top_left',
-			config: {
-				calendars: [
-					{
-						symbol: 'calendar-check-o ',
-						url: 'webcal://www.calendarlabs.com/templates/ical/US-Holidays.ics'
-					}
-				]
-			}
-		},
-		{
-			module: 'currentweather',
-			position: 'top_right',
-			config: {
-				location: 'New York',
-				locationID: '',  //ID from http://www.openweathermap.org
-				appid: 'YOUR_OPENWEATHER_API_KEY'
-			}
-		},
-		{
-			module: 'weatherforecast',
-			position: 'top_right',
-			header: 'Weather Forecast',
-			config: {
-	            location: 'New York',
-				locationID: '5128581',  //ID from http://www.openweathermap.org
-	            appid: 'YOUR_OPENWEATHER_API_KEY'
-			}
-		},
-		{
-			module: 'newsfeed',
-			position: 'bottom_bar',
-			config: {
-				feeds: [
-					{
-						title: "New York Times",
-						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
-					}
-				],
-				showSourceTitle: true,
-				showPublishDate: true
-			}
-		},
+		}
 	]
 
 };
